0b74a68efcf5aea01fc210a31dea9f7ff1c39d65
Dorgival Fernando <59142443+Dorgival-lab@users.noreply.github.com>
2020-07-05 17:08:43 -0300

    Create add two numbers.py
    Programa Python para adicionar dois números

---
+++ add two numbers.py
@@ -0,0 +1,12 @@
+# Python3 programa para adicionar dois números
+  
+number1 = input("First number: ") 
+number2 = input("\nSecond number: ") 
+  
+# Adicionando dois números 
+# O usuário também pode inserir números flutuantes 
+sum = float(number1) + float(number2) 
+  
+# Exibir a soma 
+# imprimirá o valor em float 
+print("The sum of {0} and {1} is {2}" .format(number1, number2, sum)) 
